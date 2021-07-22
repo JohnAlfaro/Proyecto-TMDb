55bd737a3182755eb79765288e9a8a6ca38f4d7c
John Alfaro <66088368+JohnAlfaro@users.noreply.github.com>
2021-07-22 12:51:18 -0600

    Final

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -102,14 +102,14 @@ export function Home() {
 
   return (
     <div className="container">
-       <div className= "d-flex justify-content-around border border-info mt-3 pt-3">
-       <div>
+       <div className= "d-flex justify-content-around border border-info mt-3 pt-3 App-header">
+       <div >
       <Link to={`/`}>
       <svg  xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6zm5-.793V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
   <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
 </svg>
-<p className = "font-weight-bold text-dark text-uppercase" >Home</p>
+<p className = "Header-text" >Home</p>
         
           </Link>
       </div>
@@ -118,7 +118,7 @@ export function Home() {
       <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-film" viewBox="0 0 16 16">
   <path d="M0 1a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V1zm4 0v6h8V1H4zm8 8H4v6h8V9zM1 1v2h2V1H1zm2 3H1v2h2V4zM1 7v2h2V7H1zm2 3H1v2h2v-2zm-2 3v2h2v-2H1zM15 1h-2v2h2V1zm-2 3v2h2V4h-2zm2 3h-2v2h2V7zm-2 3v2h2v-2h-2zm2 3h-2v2h2v-2z"/>
 </svg>
-  <p className = "font-weight-bold text-dark text-uppercase">Tendecias</p>
+  <p className = "Header-text text-second" >Más Valoradas</p>
           </Link>
       </div>
       <div>
@@ -126,7 +126,7 @@ export function Home() {
       <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" fill="currentColor" class="bi bi-gender-trans" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M0 .5A.5.5 0 0 1 .5 0h3a.5.5 0 0 1 0 1H1.707L3.5 2.793l.646-.647a.5.5 0 1 1 .708.708l-.647.646.822.822A3.99 3.99 0 0 1 8 3c1.18 0 2.239.51 2.971 1.322L14.293 1H11.5a.5.5 0 0 1 0-1h4a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V1.707l-3.45 3.45A4 4 0 0 1 8.5 10.97V13H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V14H6a.5.5 0 0 1 0-1h1.5v-2.03a4 4 0 0 1-3.05-5.814l-.95-.949-.646.647a.5.5 0 1 1-.708-.708l.647-.646L1 1.707V3.5a.5.5 0 0 1-1 0v-3zm5.49 4.856a3 3 0 1 0 5.02 3.288 3 3 0 0 0-5.02-3.288z"/>
 </svg>
-<p className = "font-weight-bold text-dark text-uppercase">Géneros</p>
+<p className = "Header-text text-second">Géneros</p>
           </Link>
       </div>
       </div>
